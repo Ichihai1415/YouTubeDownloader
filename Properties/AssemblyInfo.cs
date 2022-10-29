bef941dea8a9a33f91011e79800ce98c28bffc7f
いちはい@Project-S <98251808+Ichihai1415@users.noreply.github.com>
2022-10-30 01:10:11 +0900

    v1.0.0
    初リリース。

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("YouTubeDownloader")]
+[assembly: AssemblyDescription("yt-dlp.exeとffmpeg.exeを利用した動画のダウンロードを支援します。")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ichihai1415")]
+[assembly: AssemblyProduct("YouTubeDownloader")]
+[assembly: AssemblyCopyright("Copyright ©  2022 Ichihai1415")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("b19df15d-dc1a-4213-b6cc-0fb6dea09691")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
