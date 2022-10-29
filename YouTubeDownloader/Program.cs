60b24b1e62fa353ce3429445be5d22cfd8003b15
Project-S-31415 <projects31415.gif@gmail.com>
2022-10-29 21:20:10 +0900

    プロジェクト ファイルを追加します。

---
+++ YouTubeDownloader/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeDownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!File.Exists("yt-dlp.exe"))
+            {
+                WebClient WC = new WebClient();
+                Console.WriteLine("yt-dlp.exeをダウンロードしています...");
+                WC.DownloadFile($"https://github.com/yt-dlp/yt-dlp/releases/download/latest/yt-dlp.exe", "yt-dlp.exe");
+                Console.WriteLine("yt-dlp.exeのダウンロードが完了しました。");
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
